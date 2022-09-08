b02139012fcad83ef8f9a4c79409877397ecc149
Igor3550 <igormeloigormelo@gmail.com>
2022-09-08 02:00:43 -0300

    Layout: Pagina adicionar saída

--- src/components/OutputPage/index.js
+++ src/components/OutputPage/index.js
@@ -1,8 +1,38 @@
+import {
+  Container,
+  Title,
+  InputField,
+  SaveButton
+} from './style'
+import { useState } from 'react';
+
 const OutputPage = () => {
+
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <>
-      Nova saída
-    </>
+    <Container>
+      <Title>Nova saída</Title>
+      <form onSubmit={handleSubmit}>
+        <InputField 
+          placeholder='Valor'
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          required
+        />
+        <InputField 
+          placeholder='Descrição'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <SaveButton>Salvar saída</SaveButton>
+      </form>
+    </Container>
   )
 }
 
